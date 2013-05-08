4057c5b3552fddeb6bc685117a149c8f7ee0c5b1
nick <nick@sharpitor.com>
2013-05-08 15:36:24 +0200

    update08052013

--- search/all_63.js
+++ search/all_63.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['canvaswidth_5f',['canvasWidth_',['../class_elevation.html#a3e365f16dcebabf87d83b3204f7cffbb',1,'Elevation']]],
   ['create3dview',['create3DView',['../class_polygonal.html#a6ef71859784acf95f30f4ec121878638',1,'Polygonal']]],
+  ['createdoc',['createDoc',['../class_session_stack_sh_ph.html#ada9ac23b63a42a7bf1dff4ad4bba7c38',1,'SessionStackShPh']]],
   ['createitems',['createItems',['../class_textures_lib.html#a354c15ac017a89e5749c55a09b18295c',1,'TexturesLib']]],
   ['cu',['cu',['../class_elevation.html#adaf403b2800d725b36712eab09a10713',1,'Elevation::cu()'],['../class_handle.html#a00b203629ec267561e65add3851ecccb',1,'Handle::cu()']]]
 ];
--- search/all_73.js
+++ search/all_73.js
@@ -1,6 +1,8 @@
 var searchData=
 [
   ['scalelegend',['ScaleLegend',['../class_scale_legend.html',1,'']]],
+  ['sessiondocshph',['SessionDocShPh',['../class_session_doc_sh_ph.html',1,'']]],
+  ['sessionstackshph',['SessionStackShPh',['../class_session_stack_sh_ph.html',1,'']]],
   ['sigma',['sigma',['../class_elevation.html#af7043e28659f8adec9b68fd9b2ea5b9b',1,'Elevation']]],
   ['spinboxdelegate',['SpinBoxDelegate',['../class_spin_box_delegate.html',1,'']]],
   ['strainrelaxer',['StrainRelaxer',['../class_strain_relaxer.html',1,'']]],
